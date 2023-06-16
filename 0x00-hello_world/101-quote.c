5bc884ef2e6758acb103c8112148c831d305083f
markweb-driod <markishayajatau007@gmail.com>
2023-06-15 21:29:16 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/** main - A program that prints an output
+ * Return - 1 (Success)
+ */
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(2, message, 59);
+return (1);
+}
