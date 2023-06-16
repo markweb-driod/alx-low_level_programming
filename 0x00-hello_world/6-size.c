2c97e027478562065de39003f4ad0bcb3856915d
markweb-driod <markishayajatau007@gmail.com>
2023-06-15 21:00:12 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+prinf("Size of a char: "sizeof(a));
+printf("Size of int: "sizeof(b));
+printf("Size of a long int: "sizeof(c));
+printf("Size of a long long int: "sizeof(d));
+printf("Size of a float: "sizeof(f));
+return (0);
+}
