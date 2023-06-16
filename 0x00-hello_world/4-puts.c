61cd329f619b2d4f76c9dbd04ab7488e7f5529db
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 02:26:10 -0700

    retry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  *  main - abeg just de work
  *
- *  Return :  0 (Success)
+ *  Return:  0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like buidling a multilingual puzzle,");
+	puts("\"Programming is like buidling a multilingual puzzle");
 	return (0);
 }
