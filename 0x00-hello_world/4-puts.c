e84c87e09393ec131fb157e7378388aa0673e34b
markweb-driod <markishayajatau007@gmail.com>
2023-06-15 20:32:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#include <stdio.h>
-	int main();
-		puts("Programming is like buidling a multilingual puzzle");
-	return(0);
+#include <stdio.h>/
+/**
+ * main -A program that prints s line withpupts function
+ *
+ * Return: 0 (Success)
+ */
+	int main(void)
+{
+puts("\"Programming is like buidling a multilingual puzzle");
+return (0);
+}
