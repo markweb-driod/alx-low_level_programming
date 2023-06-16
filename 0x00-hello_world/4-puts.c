e4d798881b9f91fa996a138e5bed02fadd98b42d
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 09:48:44 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-char oracle[] = "\"Programming is like buidling a multilingual puzzle";
-puts(oracle, stderr);
+
+puts("\"Programming is like buidling a multilingual puzzle");
 
 	return (0);
 }
