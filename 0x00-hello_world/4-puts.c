38bba6af9878ba52cb53da0bc99f64b63aa2f192
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 02:13:17 -0700

    wirk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  main - Entry point
+ *  main - abeg just de work
  *
  *  Return -  0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like buidling a multilingual puzzle ,");
+	puts("\"Programming is like buidling a multilingual puzzle,");
 return (0);
 }
