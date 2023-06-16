205103ea511938a5a57cf0b2a2e64954434d8967
markweb-driod <markishayajatau007@gmail.com>
2023-06-15 21:59:08 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *  main -A program that prints s line with inpts function
  *  Return - Always 0 (Success)
  */
-	int main()
-{;
-		puts("Programming is like buidling a multilingual puzzle");
-	return(0);
+int main()
+{
+	puts("Programming is like buidling a multilingual puzzle");
+return(0);
 }
