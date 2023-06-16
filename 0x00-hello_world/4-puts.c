10c1bf07a2bc2ddc2d6d23aa620b68611d45d3f0
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 09:43:38 -0700

    halo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-       char oracle[] = "\"Programming is like buidling a multilingual puzzle";	
-	puts(oracle);
+char oracle[] = "\"Programming is like buidling a multilingual puzzle";
+puts(oracle);
 
 	return (0);
 }
