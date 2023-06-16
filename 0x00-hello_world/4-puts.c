e2d43cabc1a67668f71d5dfe28678f19aa1a60b4
markweb-driod <markishayajatau007@gmail.com>
2023-06-15 22:08:02 -0700

    commmit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *  main -A program that prints s line with inpts function
- *  Return - Always 0 (Success)
+ *  main - A program that prints s line with inpts function
+ *  Return -  0 (Success)
  */
 int main(void)
 {
