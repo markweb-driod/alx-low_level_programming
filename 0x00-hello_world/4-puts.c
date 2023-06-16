b0d70dc4102b25b1aae8c7120f84294e2f41e1e2
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 10:42:00 -0700

    cp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *  main - abeg just de work
- *
- *  Return:  0 (Success)
- */
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-puts("\"Programming is like buidling a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
