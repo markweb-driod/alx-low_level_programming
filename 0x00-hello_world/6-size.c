a1fb663dfb06b573a779cc8f89498252eef84fc3
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 13:06:02 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char:%zu byte(s)\n", sizeof(a));
-printf("Size of a int:%zu byte(s)\n", sizeof(b));
-printf("Size of a long int:%zu byte(s)\n", sizeof(c));
-printf("Size of a long long int:%zu byte(s)\n", sizeof(d));
-printf("Size of a float:%zu byte(s)\n", sizeof(f));
+printf("Size of a char :%zu byte(s)\n", sizeof(a));
+printf("Size of a int :%zu byte(s)\n", sizeof(b));
+printf("Size of a long int :%zu byte(s)\n", sizeof(c));
+printf("Size of a long long int :%zu byte(s)\n", sizeof(d));
+printf("Size of a float :%zu byte(s)\n", sizeof(f));
 return (0);
 }
