7c09b6c15ee043a1d5c707fc9cdfa4e035c2a181
markweb-driod <markishayajatau007@gmail.com>
2023-06-16 18:06:07 -0700

    com

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* main - This file creates random int */
+/* Return: Always 1 (Success) */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
