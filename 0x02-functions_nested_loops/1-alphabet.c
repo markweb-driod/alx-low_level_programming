6ec5194c08b9a6833a437585005435bbf1b0e477
markweb-driod <markishayajatau007@gmail.com>
2023-06-20 13:11:01 -0700

    whay

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main() {
+	    char letter;
+	        
+	        for (letter = 'a'; letter <= 'z'; letter++) {
+			        putchar(letter);
+				        putchar(' ');
+					    }
+		    
+		    putchar('\n');
+return 0;
