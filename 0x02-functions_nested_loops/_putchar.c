d3fe1c7b6e1f759f9b8ad423843bed1946f235ab
markweb-driod <markishayajatau007@gmail.com>
2023-06-21 11:45:39 -0700

    hpp

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - this code writes character c to stdout
+ *
+ * Return: Always 1 (Success)
+ */
+
+int _putchar(char c)
+{
+	return (wrute(1, &c, 1));
+}
