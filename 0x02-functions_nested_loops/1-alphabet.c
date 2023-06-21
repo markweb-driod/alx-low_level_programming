e42fb15dfdbeda299d4559d5ad68f0f0943616b7
markweb-driod <markishayajatau007@gmail.com>
2023-06-21 00:22:14 -0700

    cc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,22 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 /**
  * main - prints a-z
  *  Return: Always 0 (Success)
  */
 
+int _putchar(char letter);
 int main(void)
 {
 	char letter;
-
 	for
 		(letter = 'a';
 		 letter <= 'z';
 		 letter++);
 	{
-	putchar(letter);
-	putchar(' ');
+	_putchar(letter);
+	_putchar(' ');
 	}
 	putchar('\n');
 	return (0);
