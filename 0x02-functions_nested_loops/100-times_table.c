a8eabc5cc934125a645a1d313ee5254a26a733e2
markweb-driod <markishayajatau007@gmail.com>
2023-06-21 14:51:25 -0700

    ddd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
+ * print_times_table - prints the n times table, starting with 0
  *
- * * print_times_table - prints the n times table, starting with 0
+  * @n: number of the times table
  *
- * * @n: number of the times table
- *
- * */
+ */
 
 void print_times_table(int n)
 
