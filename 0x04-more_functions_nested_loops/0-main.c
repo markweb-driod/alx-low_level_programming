08b6333b49b76bfab3cce527ae2fc71793cdf4ab
markweb-driod <markishayajatau007@gmail.com>
2023-06-25 15:02:19 -0700

    dsd

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -5,12 +5,17 @@
 *
 * Return: Always 0.
 */
+
 int main(void)
 {
-char c;
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
+	char c;
+
+	c = 'A';
+
+	printf("%c: %d\n", c, _isupper(c));
+
+	c = 'a';
+
+	printf("%c: %d\n", c, _isupper(c));
 return (0);
 }
