e4df7d69c8cda1c3b795fe93cf98412c9860787a
markweb-driod <markishayajatau007@gmail.com>
2023-06-26 12:14:32 -0700

    comm

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - writing a fucntion that tkes and update value from pointer
+ *
+ * Return: 1 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory contains task on pointers arrays and strings
