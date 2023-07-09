35edd66b0d613851ba0d075e72bbffe9019361ea
markweb-driod <markishayajatau007@gmail.com>
2023-07-09 12:15:08 -0700

    second task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - THIS PROGRAM PRINTS NUMBER OF ARGUMENTS IN THE CODE
+ * @argc: shows number of arguments in codee
+ * @argv: shows number argument present in code
+ *
+ * Return: 1 Success
+ */
+
+	int main(int argc, char *argv[] __attribute__((unused)) )
+{
+	printf("%d", argc);
+
+	return (0);
+}
