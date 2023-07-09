82521a7a8ea82a44709ad71c3eb639f176e58a5e
markweb-driod <markishayajatau007@gmail.com>
2023-07-09 11:48:16 -0700

    recommitting code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - A PROGRAM THAT PRINTS ITS NAME FOLLOWED BY A NEW LINE IN C
+ * @argc: describes argument count
+ * @argv: describes contents
  * Return: 1 Success
  */
 
 int main(int argc, char *argv[])
 {
-	printf("mynameis %s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
+	return (0);
 }
